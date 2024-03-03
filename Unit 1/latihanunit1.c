25eecd31afaa5cfbdd5b78e4860d5020335b80d4
andhikaputraaaa <161611214+andhikaputraaaa@users.noreply.github.com>
2024-03-03 20:47:02 +0700

    Add files via upload

--- Unit 1/latihanunit1.c
+++ Unit 1/latihanunit1.c
@@ -3,24 +3,24 @@
 int main(){
     int v, s, t;
 
-    printf("Masukkan Jarak dan Waktu : ");
+    printf("Masukkan jarak dan waktu : ");
     scanf("%d %d", &s, &t);
 
     float kecepatan = (float) s / t;
 
-    printf("Masukkan Kecepatan dan Waktu : ");
+    printf("Masukkan kecepatan dan waktu : ");
     scanf("%d %d", &v, &t);
 
     float jarak = (float) v * t;
 
-    printf("Masukkan Jarak dan Kecepatan : ");
+    printf("Masukkan jarak dan kecepatan : ");
     scanf("%d %d", &s, &v);
 
     float waktu = (float) s / v;
 
-    printf("Kecepatannya adalah %.2f\n", kecepatan);
-    printf("Jaraknya adalah %.2f\n", jarak);
-    printf("Waktunya adalah %.2f\n", waktu);
+    printf("Kecepatan = %.2f\n", kecepatan);
+    printf("Jarak = %.2f\n", jarak);
+    printf("Waktu = %.2f\n", waktu);
 
     return 0;
 }
